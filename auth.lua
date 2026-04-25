7184394f5f4f478f7b4f4f951e3d07044f6fc537
Alone2718 <alone27181155@gmail.com>
2026-04-25 16:47:59 +0700

    Update auth.lua

--- auth.lua
+++ auth.lua
@@ -1,11 +1,7 @@
--- ==========================================
--- GOD-EYE PRO AUTH TOKEN GENERATOR 🛡️
--- ==========================================
 local Players = game:GetService("Players")
 local StarterGui = game:GetService("StarterGui")
 local LocalPlayer = Players.LocalPlayer
 
--- Put your L bozos here
 local blacklist = {
     12345678,
     87654321
@@ -20,12 +16,6 @@ for _, id in ipairs(blacklist) do
 end
 
 local ff = Instance.new("ForceField")
-ff.Visible = false -- Make sure they cant see it
-if isBlacklisted then
-    ff.Name = "Blacklist"
-else
-    ff.Name = "Whitelist"
-end
-
--- Inject into StarterGui as the auth token
+ff.Visible = false
+ff.Name = isBlacklisted and "Blacklist" or "Whitelist"
 ff.Parent = StarterGui
